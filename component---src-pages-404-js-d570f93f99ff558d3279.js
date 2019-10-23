1ddbb719fb1d40e49035f16fa562f8b0f93d4935
Travis CI User <travis@example.org>
2019-10-23 21:15:27 +0000

    Updates

--- component---src-pages-404-js-d570f93f99ff558d3279.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{229:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(228),o=n(66),r=n(227);t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement(o.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-d570f93f99ff558d3279.js.map
